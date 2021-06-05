9769daf097d32e6d7f4318c9736cfaba415669a8
Maksim Kostromin <daggerok@gmail.com>
2021-06-05 12:26:51 +0300

    Add minimal project. Add ektorp sample with test. | and a comma (C) whatthecommit.com :)

---
+++ ektorp-spring-boot-app/src/main/kotlin/com/github/daggerok/couchdb/EktorpApplication.kt
@@ -0,0 +1,11 @@
+package com.github.daggerok.couchbase.officialcouchbasejavasdk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EktorpApplication
+
+fun main(args: Array<String>) {
+    runApplication<EktorpApplication>(*args)
+}
